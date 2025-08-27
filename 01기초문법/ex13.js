f12aaa8346260fb516e201c1de4bfab986cd9c89
soontae02 <becy96@gmail.com>
2025-08-27 17:25:49 +0900

    20250827조건문

---
+++ 01기초문법/ex13.js
@@ -0,0 +1,12 @@
+//for - 1~10까지 합
+let sum = 0;
+for (let i = 0; i <= 10; i++) {
+    sum += i;
+}
+console.log("-----------------------------");
+
+//10부터 1까지 반대로 반복문 출력
+let min = 0;
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
